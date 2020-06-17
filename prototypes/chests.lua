397c8bb4358fd2cfd4a2018d0a371716472a28e9
Zombieland4942 <zombieland4942@hotmail.com>
2020-06-17 19:28:57 +1000

    Fixed Chest Graphics

--- prototypes/chests.lua
+++ prototypes/chests.lua
@@ -8,13 +8,16 @@ for x, chest in pairs(chests) do
     item = util.table.deepcopy(item_base)
 
     entity.name = chest.name    
-    --entity.icon = "__darkstar-logistics__/graphics/icons/" .. chest.name .. ".png"
+    entity.icon = "__darkstar-logistics__/graphics/icons/" .. chest.name .. ".png"
     entity.minable.result = chest.name
     entity.max_health = chest.health
     entity.inventory_size = chest.inventory_size
         
+    entity.picture.layers[1].filename = "__darkstar-logistics__/graphics/entity/" .. chest.name .. "/chest.png"
+    entity.picture.layers[1].hr_version.filename = "__darkstar-logistics__/graphics/entity/" .. chest.name .. "/hr-chest.png"
+
     item.name = chest.name
-    --item.icon = "__darkstar-logistics__/graphics/icons/" .. chest.name .. ".png"
+    item.icon = "__darkstar-logistics__/graphics/icons/" .. chest.name .. ".png"
     item.place_result = chest.name
     item.order = chest.order
     item.subgroup = "ds-storage"
